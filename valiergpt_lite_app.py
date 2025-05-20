063b2e8fe233c7a3e9f060c558906a88d4f1e1ef
VALIER <JG_SALOMON@YAHOO.FR>
2025-05-20 16:42:24 +0200

    Add files via upload

--- valiergpt_lite_app.py
+++ valiergpt_lite_app.py
@@ -66,6 +66,6 @@ Utilise un ton professionnel, concis, orienté courtier. Ne fais pas de généra
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-     import os
+    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
